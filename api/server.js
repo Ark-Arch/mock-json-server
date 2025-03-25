1c867fae41bcdaa30ac3c12f4af95954efe7e50a
Apoti-Eri117 <apotieri117@gmail.com>
2025-03-25 06:37:09 +0000

    fixed json-server

--- api/server.js
+++ api/server.js
@@ -6,7 +6,8 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
-});
+// const port = process.env.PORT || 3000;
+// server.listen(port, () => {
+//   console.log(`JSON Server is running on port ${port}`);
+// });
+module.exports = server;
